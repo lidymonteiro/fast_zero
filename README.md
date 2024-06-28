cd753a5105cfb069521d6ea37d26b81ada3effce
Lidy <lidymonteiro@JUS-202300230.local>
2024-06-28 12:48:26 -0300

    :sparkles: feat: Implementing CRUD routes

---
+++ README.md
@@ -0,0 +1,2 @@
+## FastAPI do Zero
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,65 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Ola, mundo!'}
+    assert response.json() == {'message': 'Ola, Mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'password': '1234',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername',
+        'email': 'test@test.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'testusername',
+                'email': 'test@test.com',
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'id': 1,
+            'username': 'testusername2',
+            'email': 'test@test.com',
+            'password': '123',
+        },
+    )
+
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername2',
+        'email': 'test@test.com',
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted!'}
